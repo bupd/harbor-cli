029a5562b7d02422441de50f232361460902fccf
bupd <bupdprasanth@gmail.com>
2024-05-29 02:43:22 +0530

    update: create view to support ldap and group members
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/views/member/create/view.go
+++ pkg/views/member/create/view.go
@@ -3,6 +3,7 @@ package create
 import (
 	"errors"
 	"log"
+	"strconv"
 
 	"github.com/charmbracelet/huh"
 	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
@@ -34,6 +35,18 @@ func CreateMemberView(createView *CreateView) {
 		roleSelectOptions = append(roleSelectOptions, huh.NewOption(name, id))
 	}
 
+	groupOptions := []string{"None", "LDAP group", "HTTP group", "OIDC group"}
+	var groupSelectOptions []huh.Option[int]
+	for id, name := range groupOptions {
+		groupSelectOptions = append(groupSelectOptions, huh.NewOption(name, id))
+	}
+
+	var (
+		groupType int
+		userID    string
+		groupID   string
+	)
+
 	theme := huh.ThemeCharm()
 	err := huh.NewForm(
 		huh.NewGroup(
@@ -44,12 +57,47 @@ func CreateMemberView(createView *CreateView) {
 				Options(roleSelectOptions...),
 
 			huh.NewInput().
+				Title("User ID").
+				Value(&userID).
+				Validate(func(str string) error {
+					createView.MemberUser.UserID, _ = strconv.ParseInt(str, 10, 64)
+					return nil
+				}),
+			huh.NewInput().
 				Title("Username").
 				Value(&createView.MemberUser.Username).
 				Validate(func(str string) error {
-					if str == "" {
-						return errors.New("email cannot be empty")
+					if userID == "" && str == "" {
+						return errors.New("Username and UserID cannot both be empty.")
 					}
+					return nil
+				}),
+			huh.NewInput().
+				Title("Group ID").
+				Value(&groupID).
+				Validate(func(str string) error {
+					createView.MemberGroup.ID, _ = strconv.ParseInt(str, 10, 64)
+					return nil
+				}),
+			huh.NewInput().
+				Title("Group Name").
+				Value(&createView.MemberGroup.GroupName).
+				Validate(func(str string) error {
+					return nil
+				}),
+			huh.NewSelect[int]().
+				Title("Group Type").
+				Value(&groupType).
+				Validate(func(str int) error {
+					createView.MemberGroup.GroupType = int64(str)
+					return nil
+				}).
+				Options(groupSelectOptions...),
+
+			huh.NewInput().
+				Title("DN of LDAP group").
+				Value(&createView.MemberGroup.GroupName).
+				Validate(func(str string) error {
 					return nil
 				}),
 		),
